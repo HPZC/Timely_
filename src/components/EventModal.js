0c3502123f6cbeca471532282b52721aca2da83a
Henry Phang <henryphang20007@gmail.com>
2022-09-17 11:07:02 +0800

    animation deleted

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -51,7 +51,7 @@ export default function EventModal() {
   }
   return (
     <Draggable>
-        <div className=" h-screen w-full fixed left-0 top-0  flex justify-center items-center rounded-lg">
+        <div className=" h-screen w- fixed left-0 top-0  flex justify-center items-center rounded-lg">
       <form className="bg-white rounded-lg shadow-2xl w-96">
         <header className="rounded-t-lg bg-gray-100 px-4 py-2 flex justify-between items-center">
           <span className="material-icons-outlined text-gray-400">
